0494df142ced43bdbb27d7c58876182bb6799b06
Ivan Duarte <155582035+ivanzduarte@users.noreply.github.com>
2025-04-26 13:17:59 -0300

    Delete crudJava directory

--- crudJava/src/main/java/com/api/crud/services/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.api.crud.services;
-import org.springframework.stereotype.Service;
-@Service
-public class HelloService {
-    public String getHello(String name) {
-        return "Hello %s!".formatted(name);
-    }
-}
