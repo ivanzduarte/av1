6bba9563e4a31e553a68e8e71d58129e0124df3f
Ivan Duarte <155582035+ivanzduarte@users.noreply.github.com>
2025-04-25 21:07:57 -0300

    Delete persistenciadadosav1-main directory

--- persistenciadadosav1-main/src/main/java/com/example/persistenciadadosav1/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistenciadadosav1.repository;
-
-import com.example.persistenciadadosav1.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
--- persistenciadadosav1-main/src/main/java/com/example/persistenciadadosav1/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistenciadadosav1.repository;
-
-import com.example.persistenciadadosav1.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
